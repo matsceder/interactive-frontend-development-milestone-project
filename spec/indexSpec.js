688164015b96eafa34c9d4905728c975fe2fa64b
matsceder <mats_ceder@hotmail.com>
2020-03-21 12:32:06 +0000

    simplified test for modal and tested onoff button trigger clearcolor

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -3,14 +3,17 @@
  
 /* ------------- Test Modal Open Close ------------- */
 describe("Modal open close", () => {
-    describe("btnModal click", () => {
+/*    describe("btnModal click to change css style", () => {
         it("should have css display block", () => {
             spyOnEvent($(btnModal), "click")
             $(btnModal).click()
             expect($("#rulesModal")).toHaveCss({"display": "block"})
         })
-    })
-    describe("btnModalClose click", () => {
+    })*/
+    describe("btnModalClose click to change css style", () => {
+        beforeEach(function() {
+            $(btnModal).click()
+        })
         it("should have css display none", () => {
             spyOnEvent($(btnModalClose), "click")
             $(btnModalClose).click()
@@ -19,14 +22,21 @@ describe("Modal open close", () => {
     })
 });
 
-/* ------------- Test  ------------- */
-/*describe("", () => {
-    describe("", () => {
-        it("", () {
-            expect().()
+/* ------------- Test On button ------------- */
+describe("OnOff button", () => {
+    describe("Turning the game off", () => {
+        
+        beforeEach(function() {
+            $(onToggleBtn).click();
+        })
+
+        it("Should call clearColor and clearInterval", () => {
+            spyOn(window, "clearColor");
+            $(onToggleBtn).click();
+            expect(window.clearColor).toHaveBeenCalled()
         })
     })
 })
-*/
+ 
 
 
